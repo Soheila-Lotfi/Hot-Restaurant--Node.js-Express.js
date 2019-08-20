55be97c5de28d7599683f10f443fa170cda83b28
soheila <soheilalotfi@ymail.com>
2019-08-20 15:19:38 -0400

    use ajax instead of get in runWaitListQuery function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.post("/api/tables", function(req, res) {
     var newTable = req.body;
 
-    if (tables.length <= 5) {
+    if (tables.length < 2) {
       tables.push(newTable);
       return res.json(true); // we set up boolean value here, so we can trigger a defualt message to the user ;;alert in the reserve.html
     } else {
